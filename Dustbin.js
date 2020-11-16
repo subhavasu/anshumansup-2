482aa24211c294958015bb9eee84f6ebfb395a35
subhavasu <70517919+subhavasu@users.noreply.github.com>
2020-11-16 08:55:12 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,31 @@
+class dustbin {
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true
+        }
+        
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+       
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("red")
+        rect(0,0,this.width,this.height);
+        pop();
+    } 
+}
+
+    
+
+
+
+
+
+
